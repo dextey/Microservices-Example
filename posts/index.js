5549881cf2564730267cd26006179ee403372ee0
Dexter <dexterdevmode@gmail.com>
2022-07-12 12:00:01 +0530

    added service functionalities

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -12,8 +14,8 @@ app.get("/posts", (req, res) => {
 });
 app.post("/posts", (req, res) => {
   const id = randomBytes(4).toString("hex");
-  const { title } = req.body;
-  posts[id] = { id, title };
+  const { post } = req.body;
+  posts[id] = { id, post };
 
   res.status(201).send(posts[id]);
 });
