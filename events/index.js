c3e9a3aca6adb90bbeb4972a3dcaf522dbf76896
Dexter <dexterdevmode@gmail.com>
2022-07-13 08:31:58 +0530

    Event bus initiated

---
+++ events/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/events/", (req, res) => {
+  const event = req.body;
+
+  axios
+    .post("http://localhost:4000/events", event)
+    .catch((error) => console.log("error 1"));
+  axios
+    .post("http://localhost:4001/events", event)
+    .catch((error) => console.log("error 2"));
+  axios
+    .post("http://localhost:4002/events", event)
+    .catch((error) => console.log("error 3"));
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("EventBus listening at port 4005");
+});
